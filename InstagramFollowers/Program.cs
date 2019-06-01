54a8e9ba5d07bfa2881287e914ce1e4ccb048a7c
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-06-01 22:44:43 +0300

    First Commit

---
+++ InstagramFollowers/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+
+namespace InstagramFollowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread Sleep Time
+            int time = 3000; //ms
+
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"D:\Selenium"); //GeckoDriver
+            service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe"; //FireFox Path
+
+            IWebDriver driver = new FirefoxDriver(service);
+            string url = @"https://www.instagram.com/accounts/login/?source=auth_switcher";
+            driver.Navigate().GoToUrl(url);
+            Thread.Sleep(time);
+            //Login
+            driver.FindElement(By.Name("username")).SendKeys("instagram@instagram.com"); //Commit Etmeden Önce Mutlaka Sil
+            driver.FindElement(By.Id("password")).SendKeys("mypassword"); //Committen Önce Sil
+            driver.FindElement(By.XPath("//*[@id='react-root']/section/main/div/article/div/div[1]/div/form/div[6]")).Click();
+            //Login End
+        }
+    }
+}
