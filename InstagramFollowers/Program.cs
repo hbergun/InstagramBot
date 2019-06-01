0f6230402d4dff5b0cf5e645211aee5c3e4e44f8
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-06-01 22:52:39 +0300

    Login Completed

--- InstagramFollowers/Program.cs
+++ InstagramFollowers/Program.cs
@@ -19,10 +19,11 @@ namespace InstagramFollowers
             driver.Navigate().GoToUrl(url);
             Thread.Sleep(time);
             //Login
-            driver.FindElement(By.Name("username")).SendKeys("instagram@instagram.com"); //Commit Etmeden Önce Mutlaka Sil
-            driver.FindElement(By.Id("password")).SendKeys("mypassword"); //Committen Önce Sil
-            driver.FindElement(By.XPath("//*[@id='react-root']/section/main/div/article/div/div[1]/div/form/div[6]")).Click();
+            driver.FindElement(By.Name("username")).SendKeys("instagram@instagram.com"); //Your İnstagram UserName Or Email Or Phone Number
+            driver.FindElement(By.Name("password")).SendKeys("mypassword"); //Your İnstagram Password
+            driver.FindElement(By.CssSelector("div.Igw0E:nth-child(4)")).Click();
             //Login End
+
         }
     }
 }
