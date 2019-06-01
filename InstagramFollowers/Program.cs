e89017b621f00ba8e1533662e183009bb9e1c389
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-06-01 23:17:16 +0300

    Followers And Following Count Getting

--- InstagramFollowers/Program.cs
+++ InstagramFollowers/Program.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace InstagramFollowers
@@ -19,11 +21,25 @@ namespace InstagramFollowers
             driver.Navigate().GoToUrl(url);
             Thread.Sleep(time);
             //Login
-            driver.FindElement(By.Name("username")).SendKeys("instagram@instagram.com"); //Your İnstagram UserName Or Email Or Phone Number
-            driver.FindElement(By.Name("password")).SendKeys("mypassword"); //Your İnstagram Password
+            driver.FindElement(By.Name("username")).SendKeys("Myusername"); //Your İnstagram UserName Or Email Or Phone Number
+            driver.FindElement(By.Name("password")).SendKeys("MyPassword"); //Your İnstagram Password
             driver.FindElement(By.CssSelector("div.Igw0E:nth-child(4)")).Click();
             //Login End
 
+            //Go To Profile Page
+            driver.Navigate().GoToUrl(@"https://www.instagram.com/ergunbh/"); //UserName
+            //Profile Page Show
+
+            //Show Followers Page
+            driver.Navigate().GoToUrl(@"https://www.instagram.com/ergunbh/followers/");
+            //Followers Page Opened
+
+            //Get Followers And Following User Count
+            var followersCount = driver.FindElement(By.CssSelector("li.Y8-fY:nth-child(2) > a:nth-child(1) > span:nth-child(1)")).Text;
+            var followingCount = driver.FindElement(By.CssSelector("li.Y8-fY:nth-child(3) > a:nth-child(1) > span:nth-child(1)")).Text;
+
+           
+            Console.Read();
         }
     }
 }
